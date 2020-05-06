d36a03f90eff07334d8d327975451f805809ba02
Mohamed AZOUAOUI <azouaoui.med@gmail.com>
2020-05-06 15:38:24 +0100

    Updates

---
+++ precache-manifest.01a4ae5c153b404529ec14506d75dc32.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "167b8e7a6895d900104c4ea2aa0e71cf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1e155fc999c800ff8d33",
+    "url": "./static/css/2.6be917c3.chunk.css"
+  },
+  {
+    "revision": "32240589035ec76a08c7",
+    "url": "./static/css/main.2fb83259.chunk.css"
+  },
+  {
+    "revision": "1e155fc999c800ff8d33",
+    "url": "./static/js/2.58c5905e.chunk.js"
+  },
+  {
+    "revision": "8bc88370bc2489d86f2f357f447ddb0b",
+    "url": "./static/js/2.58c5905e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32240589035ec76a08c7",
+    "url": "./static/js/main.1e88fd61.chunk.js"
+  },
+  {
+    "revision": "caa923266a42741ac64b",
+    "url": "./static/js/runtime-main.a6972c87.js"
+  },
+  {
+    "revision": "74aaeeb9757d6af381f302c7800647f5",
+    "url": "./static/media/bg1.74aaeeb9.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3fb637d6768e892207108c731c382bb0.js"
+  "./precache-manifest.01a4ae5c153b404529ec14506d75dc32.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
