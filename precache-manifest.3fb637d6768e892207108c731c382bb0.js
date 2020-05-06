f6d3b9b3cb8931d2c5be3d17d85b929b969937ce
Mohamed AZOUAOUI <azouaoui.med@gmail.com>
2020-05-06 15:26:07 +0100

    Updates

---
+++ precache-manifest.3fb637d6768e892207108c731c382bb0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b1e2a3a7667725de308909406a57091",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e155fc999c800ff8d33",
+    "url": "/static/css/2.6be917c3.chunk.css"
+  },
+  {
+    "revision": "32240589035ec76a08c7",
+    "url": "/static/css/main.2fb83259.chunk.css"
+  },
+  {
+    "revision": "1e155fc999c800ff8d33",
+    "url": "/static/js/2.58c5905e.chunk.js"
+  },
+  {
+    "revision": "8bc88370bc2489d86f2f357f447ddb0b",
+    "url": "/static/js/2.58c5905e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32240589035ec76a08c7",
+    "url": "/static/js/main.1e88fd61.chunk.js"
+  },
+  {
+    "revision": "5c4aaab1c54dd800f5a6",
+    "url": "/static/js/runtime-main.e3780c05.js"
+  },
+  {
+    "revision": "74aaeeb9757d6af381f302c7800647f5",
+    "url": "/static/media/bg1.74aaeeb9.jpg"
+  }
+]);
