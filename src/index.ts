d33cc79dbec087547dfe0911cd5a03b8bb2a0501
Mohamed AZOUAOUI <azouaoui.med@gmail.com>
2020-05-21 15:30:15 +0100

    Refactor project structure

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export { default as ProSidebar } from './ProSidebar/ProSidebar';
-export { default as SidebarHeader } from './ProSidebar/SidebarHeader';
-export { default as SidebarContent } from './ProSidebar/SidebarContent';
-export { default as SidebarFooter } from './ProSidebar/SidebarFooter';
-export { default as Menu } from './ProSidebar/Menu';
-export { default as MenuItem } from './ProSidebar/MenuItem';
-export { default as SubMenu } from './ProSidebar/SubMenu';
+export { default as SidebarHeader } from './ProSidebar/Layout/SidebarHeader';
+export { default as SidebarContent } from './ProSidebar/Layout/SidebarContent';
+export { default as SidebarFooter } from './ProSidebar/Layout/SidebarFooter';
+export { default as Menu } from './ProSidebar/Menu/Menu';
+export { default as MenuItem } from './ProSidebar/Menu/MenuItem';
+export { default as SubMenu } from './ProSidebar/Menu/SubMenu';
