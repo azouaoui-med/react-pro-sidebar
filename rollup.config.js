395737b4acb40cf270368b436e08b633902d02fe
Mohamed Azouaoui <azouaoui.med@gmail.com>
2021-12-04 19:18:45 +0100

    setup eslint for v1

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,6 @@ import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
-import scss from 'rollup-plugin-scss';
-import copy from 'rollup-plugin-copy';
 
 import packageJson from './package.json';
 
@@ -13,12 +11,10 @@ export default {
     {
       file: packageJson.main,
       format: 'cjs',
-      // sourcemap: true,
     },
     {
       file: packageJson.module,
       format: 'esm',
-      // sourcemap: true,
     },
   ],
   plugins: [
@@ -26,18 +22,6 @@ export default {
     resolve(),
     commonjs(),
     typescript({ useTsconfigDeclarationDir: true }),
-    scss({
-      output: 'dist/css/styles.css',
-      watch: 'src/scss',
-    }),
-    copy({
-      targets: [
-        {
-          src: 'src/scss',
-          dest: 'dist',
-        },
-      ],
-    }),
   ],
   external: ['react', 'react-dom', 'prop-types'],
 };
