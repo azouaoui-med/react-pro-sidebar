5514fa3fa6bc3ab1625f3602a3ca95dd62c8a38d
Mohamed Azouaoui <azouaoui.med@gmail.com>
2022-09-03 21:58:57 +0100

    update playground story

--- storybook/Playground.tsx
+++ storybook/Playground.tsx
@@ -4,21 +4,49 @@ import { Menu, MenuItem, SubMenu, useProSidebar } from '../src';
 import { Icon } from './icons/Icon';
 
 export const Playground: React.FC = () => {
-  const { toggleSidebar, collapseSidebar, broken } = useProSidebar();
+  const { toggleSidebar, collapseSidebar, broken, rtl } = useProSidebar();
+
+  const handleClick = () => {
+    console.log('onclick');
+  };
 
   return (
-    <div style={{ display: 'flex', height: '100%' }}>
+    <div style={{ display: 'flex', height: '100%', direction: rtl ? 'rtl' : 'ltr' }}>
       <Sidebar
         breakPoint="always"
         customBreakPoint="1000px"
         transitionDuration={200}
-        // backgroundColor="rgba(27, 38, 58, 1)"
+        // backgroundColor="rgba(27, 38, 58, 0.7)"
         // image="https://i.pinimg.com/736x/8e/6c/06/8e6c064f57f94838263d7ba9ad80f353.jpg"
       >
         <div>
-          <Menu>
-            <SubMenu label="Charts" icon={<Icon name="bar-chart" />}>
-              <MenuItem> Pie charts</MenuItem>
+          <Menu
+          // closeOnClick
+          // renderExpandIcon={({ level, collapsed, open }) => (
+          //   <span>{collapsed && level === 0 ? 'o' : open ? '-' : '+'}</span>
+          // )}
+          // renderMenuItemStyles={({ collapsed, level }) => ({
+          //   color: level === 0 ? 'blue' : 'red',
+          //   '.menu-icon': {
+          //     backgroundColor: '#e1e1e1',
+          //   },
+          //   // '.menu-anchor': {
+          //   //   color: level === 0 ? 'blue' : 'red',
+          //   // },
+          //   '&.sub-menu': {
+          //     '>.sub-menu-content': {
+          //       ...(level === 0 &&
+          //         {
+          //           // backgroundColor: '#e1e1e150',
+          //           // maxHeight: '200px',
+          //           // overflow: 'auto',
+          //         }),
+          //     },
+          //   },
+          // })}
+          >
+            <SubMenu active label="Charts" icon={<Icon name="bar-chart" />} prefix="OK" suffix="🔥">
+              <MenuItem onClick={handleClick}> Pie charts</MenuItem>
               <MenuItem> Line charts</MenuItem>
               <MenuItem> Bar charts</MenuItem>
             </SubMenu>
@@ -42,7 +70,7 @@ export const Playground: React.FC = () => {
                 </SubMenu>
               </SubMenu>
             </SubMenu>
-            <MenuItem icon={<Icon name="book-2" />}> Documentation</MenuItem>
+            <MenuItem icon={<Icon name="book-2" />}>Documentation</MenuItem>
             <MenuItem icon={<Icon name="calendar" />}> Calendar</MenuItem>
             <MenuItem icon={<Icon name="shopping-cart" />}> E-commerce</MenuItem>
             <MenuItem icon={<Icon name="service" />}> Examples</MenuItem>
