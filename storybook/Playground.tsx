54983d74122203c4ca9bfa6cbae27eecc3a15eec
Mohamed Azouaoui <azouaoui.med@gmail.com>
2023-01-04 18:39:51 +0100

    add dark theme example to playground

--- storybook/Playground.tsx
+++ storybook/Playground.tsx
@@ -1,23 +1,92 @@
 import React from 'react';
-import { Sidebar, Menu, MenuItem, SubMenu, useProSidebar } from '../src';
+import { Sidebar, Menu, MenuItem, SubMenu, useProSidebar, CSSObject, menuClasses } from '../src';
 import { Switch } from './components/Switch';
 import { Icon } from './icons/Icon';
+
+const theme = {
+  sidebar: {
+    backgroundColor: '#0b2948',
+    color: '#8ba1b7',
+  },
+  menu: {
+    menuContent: '#082440',
+    hover: {
+      backgroundColor: '#0e3052',
+      color: '#b6c8d9',
+    },
+    active: {
+      backgroundColor: '#13395e',
+      color: '#b6c8d9',
+    },
+    disabled: {
+      color: '#3e5e7e',
+    },
+  },
+};
 
 export const Playground: React.FC = () => {
   const { toggleSidebar, collapseSidebar, broken } = useProSidebar();
 
   const [isRTL, setIsRTL] = React.useState<boolean>(false);
+  const [darkTheme, setDarkTheme] = React.useState<boolean>(false);
 
   // handle on RTL change event
   const handleRTLChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setIsRTL(e.target.checked);
   };
 
+  //handle on dark theme change event
+  const handleDarkThemeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDarkTheme(e.target.checked);
+  };
+
   return (
     <div style={{ display: 'flex', height: '100%', direction: isRTL ? 'rtl' : 'ltr' }}>
-      <Sidebar rtl={isRTL} breakPoint="lg">
+      <Sidebar
+        rtl={isRTL}
+        breakPoint="lg"
+        {...(darkTheme
+          ? {
+              backgroundColor: theme.sidebar.backgroundColor,
+              rootStyles: {
+                color: theme.sidebar.color,
+              },
+            }
+          : undefined)}
+      >
         <div>
-          <Menu>
+          <Menu
+            {...(darkTheme
+              ? {
+                  rootStyles: {
+                    [`.${menuClasses.subMenuContent}`]: {
+                      backgroundColor: theme.menu.menuContent,
+                    },
+                  },
+                  menuItemStyles: {
+                    button: {
+                      '&:hover': {
+                        backgroundColor: theme.menu.hover.backgroundColor,
+                        color: theme.menu.hover.color,
+                      },
+                    },
+                    root: {
+                      [`&.${menuClasses.active}`]: {
+                        [`.${menuClasses.button}`]: {
+                          backgroundColor: theme.menu.active.backgroundColor,
+                          color: theme.menu.active.color,
+                        },
+                      },
+                      [`&.${menuClasses.disabled}`]: {
+                        [`.${menuClasses.button}`]: {
+                          color: theme.menu.disabled.color,
+                        },
+                      },
+                    },
+                  },
+                }
+              : undefined)}
+          >
             <SubMenu label="Charts" icon={<Icon name="bar-chart" />}>
               <MenuItem> Pie charts</MenuItem>
               <MenuItem> Line charts</MenuItem>
@@ -43,8 +112,12 @@ export const Playground: React.FC = () => {
                 </SubMenu>
               </SubMenu>
             </SubMenu>
-            <MenuItem icon={<Icon name="book-2" />}>Documentation</MenuItem>
-            <MenuItem icon={<Icon name="calendar" />}> Calendar</MenuItem>
+            <MenuItem active icon={<Icon name="book-2" />}>
+              Documentation (active)
+            </MenuItem>
+            <MenuItem disabled icon={<Icon name="calendar" />}>
+              Calendar (disabled)
+            </MenuItem>
             <MenuItem icon={<Icon name="shopping-cart" />}> E-commerce</MenuItem>
             <MenuItem icon={<Icon name="service" />}> Examples</MenuItem>
           </Menu>
@@ -69,8 +142,16 @@ export const Playground: React.FC = () => {
             ) : null}
           </div>
 
-          <div style={{ padding: '0 8px' }}>
+          <div style={{ padding: '0 8px', marginBottom: 16 }}>
             <Switch id="rtl" checked={isRTL} onChange={handleRTLChange} label="RTL" />
+          </div>
+          <div style={{ padding: '0 8px' }}>
+            <Switch
+              id="theme"
+              checked={darkTheme}
+              onChange={handleDarkThemeChange}
+              label="Dark theme"
+            />
           </div>
         </div>
       </main>
