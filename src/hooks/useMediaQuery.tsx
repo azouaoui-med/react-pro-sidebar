15f6b96a4ff95bb868f9eedfa4fadd68f3c67d3f
Mohamed Azouaoui <azouaoui.med@gmail.com>
2023-05-09 20:49:44 +0100

    refactor useMediaQuery

--- src/hooks/useMediaQuery.tsx
+++ src/hooks/useMediaQuery.tsx
@@ -8,12 +8,17 @@ export const useMediaQuery = (breakpoint?: string): boolean => {
   React.useEffect(() => {
     if (breakpoint && breakpoint !== 'always') {
       const media = window.matchMedia(`(max-width: ${breakpoint})`);
-      if (media.matches !== matches) {
-        setMatches(media.matches);
-      }
-      const listener = () => setMatches(media.matches);
-      window.addEventListener('resize', listener);
-      return () => window.removeEventListener('resize', listener);
+
+      const handleMatch = () => {
+        if (media.matches !== matches) {
+          setMatches(media.matches);
+        }
+      };
+
+      handleMatch();
+
+      window.addEventListener('resize', handleMatch);
+      return () => window.removeEventListener('resize', handleMatch);
     }
   }, [matches, breakpoint]);
 
